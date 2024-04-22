4ce629b9449407ff3f96d5c2bc034c0ffb6ffc4b
Ali Taha <aliestaha04@gmail.com>
2024-04-22 16:29:12 -0400

    Larger gifs

--- gifConverter.py
+++ gifConverter.py
@@ -7,7 +7,7 @@ def convert_mp4_to_gif(input_path, output_path):
         'ffmpeg',
         '-i', input_path,  # Input file
         # Frame rate and scale using lanczos filtering
-        '-vf', 'fps=10,scale=320:-1:flags=lanczos',
+        '-vf', 'fps=10,scale=500:-1:flags=lanczos',
         '-c:v', 'gif',  # Output format as GIF
         '-f', 'gif',  # Force output to be GIF even if more suitable format exists
         output_path  # Output file
